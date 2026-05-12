a4bcf49d624cb1e5276f05f6e081db730aadf478
vbcoaching56mt-hue <vbcoaching56mt@gmail.com>
2026-05-12 15:16:38 +0200

    Update index.js

--- vb-logiciel/src/index.js
+++ vb-logiciel/src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignupPage from './pages/Signup';
+import SetupOrganisationPage from './pages/SetupOrganisation';
 import reportWebVitals from './reportWebVitals';
+
+const path = window.location.pathname;
+
+let PageComponent = App;
+if (path === '/signup') PageComponent = SignupPage;
+if (path === '/setup-organisation') PageComponent = SetupOrganisationPage;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PageComponent />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
